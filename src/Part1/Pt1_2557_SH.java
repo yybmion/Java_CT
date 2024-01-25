c020519e94c3e4d9465f120241529b8952830f21
Seohyeon Park <hyeon99@ajou.ac.kr>
2024-01-25 23:55:07 +0900

    SH's first masterpiece

---
+++ src/Part1/Pt1_2557_SH.java
@@ -0,0 +1,19 @@
+package Part1;
+
+public class Pt1_2557_SH {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
+
+
+/**
+ * -Comment-
+ * World의 대소문자 구분을 놓쳐서 5번 넘게 시도했다는....ㅠ
+ * world, World ... 잘 보자..ㅎ
+ */
+
