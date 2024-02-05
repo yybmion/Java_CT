d30eb838878676c1043c25313b4bea0cc359d142
llocr <36-96@naver.com>
2024-02-05 16:24:08 +0900

    [HS] Part1 : 2257

---
+++ src/Part1/Pt1_2257_HS.java
@@ -0,0 +1,11 @@
+package Part1;
+
+public class Pt1_2257_HS {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/* TODO : 24.02.05
+ * 맞으니까 기분 조와요~ :)
+ * */
