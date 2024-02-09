47cd1ad66929937622a3a57cc99fa6172a0a1792
MinhoJJang <ahchjang@naver.com>
2024-02-10 01:56:31 +1100

    [SOLVED] 곱셈 [B3]

---
+++ src/Part1/BOJ2588/Pt1_2588_장민호.java
@@ -0,0 +1,23 @@
+package Part1.BOJ2588;
+
+import java.util.Scanner;
+
+public class Pt1_2588_장민호 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int submul_3 = A * (B % 10);
+        int submul_4 = A * ((B / 10) % 10);
+        int submul_5 = A * (B / 100);
+
+        int total = submul_3 + submul_4 * 10 + submul_5 * 100;
+
+        System.out.println(submul_3);
+        System.out.println(submul_4);
+        System.out.println(submul_5);
+        System.out.println(total);
+    }
+}
