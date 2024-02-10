8b9ebc7a0fe76e3ac2e107d31a89ccff07898a5c
이준상 <66434787+samgashyeong@users.noreply.github.com>
2024-02-10 22:34:59 +0900

    Update Pt1_8958_이준상.java
    8958 주석 추가

--- src/Part1/BOJ8958/Pt1_8958_이준상.java
+++ src/Part1/BOJ8958/Pt1_8958_이준상.java
@@ -9,7 +9,9 @@ public class Main {
 
         for(int i = 0;i<a;i++){
             String inputS = br.readLine();
+            //각 입력마다의 합
             int sum = 0;
+            //O가 맞출 때마다 하나씩 수가 올라가는 변수
             int stack = 1;
             for(int j = 0;j<inputS.length();j++){
                 if(inputS.charAt(j) == 'O'){
