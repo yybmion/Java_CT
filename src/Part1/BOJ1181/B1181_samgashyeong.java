52f421fab75c89a695697248635c5b007532371c
이준상 <66434787+samgashyeong@users.noreply.github.com>
2024-03-22 20:30:40 +0900

    Update B1181_samgashyeong.java

--- src/Part1/BOJ1181/B1181_samgashyeong.java
+++ src/Part1/BOJ1181/B1181_samgashyeong.java
@@ -1,12 +1,16 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.nio.Buffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
-
-public class Main {
-    static boolean[][] arr = new boolean[15][15];
+import java.util.Set;
+import java.util.StringTokenizer;
+ 
+public class B1181_samgashyeong {
 
 	public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -40,5 +44,9 @@ public class Main {
         for(int i=0;i<arr.size();i++){
             System.out.println(arr.get(i));
         }
+        
+
+
+
 	}
 }
